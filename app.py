d45ff3c30cc886c9ba624a801a17c9145ebb0e3a
The RituRajPS <theriturajps@gmail.com>
2023-03-01 01:07:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 def hello_world():
    return 'Hello, World!'
 
-os.system("git clone https://github.com/theriturajps/TPB-Torrent-Bot ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 torrent.py &")
+os.system("git clone https://github.com/theriturajps/TPB-Torrent-Bot-Deploy ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 torrent.py &")
